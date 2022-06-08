4b71b7bd542997020edf9b1f9d11f2e68e2923a0
MoOlUr20 <107134027+MoOlUr20@users.noreply.github.com>
2022-06-08 23:44:55 +0700

    Add files via upload

---
+++ WWG_5/WWG_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace WWG_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+                Console.Write(" " + array[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int newPozition = rnd.Next(i);
+                int keeper = array[i];
+                array[i] = array[newPozition];
+                array[newPozition] = keeper;
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+        }
+    }
+}
