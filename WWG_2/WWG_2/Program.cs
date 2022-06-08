795bc986c38a8e7dce0bc3d18933832eca52fec7
MoOlUr20 <107134027+MoOlUr20@users.noreply.github.com>
2022-06-08 23:43:56 +0700

    Add files via upload

---
+++ WWG_2/WWG_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace WWG_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите день: ");
+            int a = int.Parse(Console.ReadLine()); 
+            Console.Write("Введите месяц: ");
+            int b = int.Parse(Console.ReadLine()); 
+            Console.Write("Введите год: ");
+            int c = int.Parse(Console.ReadLine()); 
+            var julian = new DateTime(c, b, a);
+            Console.WriteLine($"По юлианскому календарю: {julian}");
+            var grigorian = julian.AddDays( (c / 100) - ( (c / 100) / 4) - 2);
+            Console.WriteLine($"По григорианскому календарю: {grigorian}");
+        }
+    }
+}
---
+++ WWG_3/WWG_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WWG_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите год: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Даты последних четвергов года {a} ");
+            for (int i = 1; i < 12 + 1; i++)
+            {
+                DateTime d = new DateTime(a, i, DateTime.DaysInMonth(a, i));
+                Console.WriteLine(d.Day - (7 + (int)d.DayOfWeek - 4) % 7);
+            }
+        }
+    }
+}
